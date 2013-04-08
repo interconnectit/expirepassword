9d72e988f5153f629db7a5be1f35b1d2a96527a1
shrkey <team@shrkey.com>
2013-04-08 20:49:47 +0100

    Initial plugin structure

---
+++ forcepasswordchange.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Plugin Name: Force Password Change
+Version: 1.0
+Plugin URI: https://github.com/shrkey/forcepasswordchange
+Description: Forces a user to change their password, can be set to force a password change for a new user on first sign in
+Author: Shrkey
+Author URI: http://shrkey.com
+Text_domain: shrkey
+
+Copyright 2013 (email: team@shrkey.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+// Include the plugin functions
+require_once('includes/functions.php');
+
+if( is_admin() ) {
+	require_once('classes/admin.forcepasswordchange.php');
+}
+
+// We need to add this part all the time
+require_once('classes/public.forcepasswordchange.php');
