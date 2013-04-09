002d1f2b6046b4be36f86e5b0e922c5b14185767
shrkey <team@shrkey.com>
2013-04-09 22:25:19 +0100

    added code to process password change

--- classes/public.expirepassword.php
+++ classes/public.expirepassword.php
@@ -4,7 +4,13 @@ if( !class_exists( 'expirepasswordpublic') ) {
 
 	class expirepasswordpublic {
 
+		var $db;
+
 		function __construct() {
+
+			global $wpdb;
+
+			$this->db =& $wpdb;
 
 			// Load the translation file
 			add_action( 'plugins_loaded', array(&$this, 'load_textdomain'));
@@ -29,40 +35,103 @@ if( !class_exists( 'expirepasswordpublic') ) {
 
 		}
 
-		function check_password_reset_key($key, $login) {
-			global $wpdb;
+		function reset_expired_password( $user, $new_password ) {
 
-			$key = preg_replace('/[^a-z0-9]/i', '', $key);
+			// Allow other actions to be fired - mirroring the standard reset password functionality
+			do_action( 'expired_password_reset', $user, $new_password );
 
-			if ( empty( $key ) || !is_string( $key ) )
-				return new WP_Error('invalid_key', __('Invalid key'));
+			// Reset the password for the user
+			wp_set_password( $new_password, $user->ID );
 
-			if ( empty($login) || !is_string($login) )
-				return new WP_Error('invalid_key', __('Invalid key'));
+		}
 
-			$user = $wpdb->get_row($wpdb->prepare("SELECT * FROM $wpdb->users WHERE user_activation_key = %s AND user_login = %s", $key, $login));
+		function get_users_password_hash( $user_id ) {
 
-			if ( empty( $user ) )
-				return new WP_Error('invalid_key', __('Invalid key'));
+			$sql = $this->db->prepare( "SELECT user_pass FROM {$this->db->users} WHERE ID = %d", $user_id );
 
-			return $user;
+			return $this->db->get_var( $sql );
+
 		}
 
 		function process_expired_password() {
 
+			// Process the expired password
+
+			// Create an errors object for us to use
+			$errors = new WP_Error();
+
+			$user_name = sanitize_user( $_POST['user_login'] );
+			if ( $user = get_user_by('login', $user_name) ) {
+				// User exists - check the key is valid
+				// Check the passwords have been entered and that they match
+				if ( isset($_POST['pass1']) && $_POST['pass1'] != $_POST['pass2'] ) {
+					$errors->add( 'password_reset_mismatch', __( 'The passwords do not match.', 'expirepassword' ) );
+				} else {
+					// Check the password isn't the same as the last one
+					$existingpassword = $this->get_users_password_hash( $user->ID );
+					if( wp_check_password( $_POST['pass1'], $existingpassword ) ) {
+						// The password matches - we don't want them setting the same password as before...
+						$errors->add( 'password_reset_sameh', __( 'Please choose a different password from your previous one.', 'expirepassword' ) );
+					} else {
+						// Get the stored key
+						$thekey = shrkey_get_usermeta_oncer( $user->ID, '_shrkey_password_expired_key' );
+						// Get and parse the passed key
+						$passedkey = preg_replace('/[^a-z0-9]/i', '', $_POST['key']);
+						if( !empty( $thekey) && !empty($passedkey) && $thekey == $passedkey ) {
+							// The key is valid as well - so we need to reset these passwords
+							$this->reset_expired_password( $user, $_POST['pass1'] );
+							// Remove the expired key setting
+							shrkey_delete_usermeta_oncer( $user->ID, '_shrkey_password_expired' );
+
+							// Send the user back to the login
+							login_header( __( 'Password Reset' ), '<p class="message reset-pass">' . __( 'Your password has been reset, please login again.', 'expirepassword' ) . ' <a href="' . esc_url( wp_login_url() ) . '">' . __( 'Log in', 'expirepassword' ) . '</a></p>' );
+							login_footer();
+							exit();
+
+						} else {
+							// The key either doesn't exist or doesn't match
+							$errors->add( 'password_expired_nokey', __( 'Could not change password, please try again.', 'expirepassword' ) );
+						}
+					}
+				}
+			} else {
+				// The key either doesn't exist or doesn't match
+				$errors->add( 'password_expired_nouser', __( 'Could not change password, please try again.', 'expirepassword' ) );
+			}
+
+			// Check for some errors here
+			if( $errors->get_error_code() ) {
+				// If we don't have a user record create a fake one
+				if( !isset($user) || is_wp_error( $user ) ) {
+					$user = '';
+				}
+				// show the reset form again
+				$this->show_reset_password_form( $user, wp_generate_password(20, false), (isset($_POST['redirect_to'])) ? $_POST['redirect_to'] : false, $errors );
+
+			}
+
+			exit();
+
 		}
 
-		function show_reset_password_form( $user, $oncerkey, $redirectto = false ) {
+		function show_reset_password_form( $user, $oncerkey, $redirectto = false, $errors = false ) {
+
+			if ( !is_a($user, 'WP_User') ) {
+				// Ooops we don't have a user to use :( return to the login form as this shouldn't happen except in hack attempts
+				wp_safe_redirect( wp_login_url() );
+				exit();
+			}
 
 			// We are going to save our key to a oncer for later checking
 			shrkey_set_usermeta_oncer( $user->ID, '_shrkey_password_expired_key', $oncerkey );
 
-			login_header( __('Expired Password', 'expirepassword'), '<p class="message reset-pass">' . __('Your password has <strong>expired</strong>. Enter a new password below.', 'expirepassword') . '</p>' );
+			login_header( __('Expired Password', 'expirepassword'), '<p class="message reset-pass">' . __('Your password has <strong>expired</strong>. Enter a new password below.', 'expirepassword') . '</p>', $errors );
 
 			?>
 			<form name="expiredpasswordform" id="expiredpasswordform" method="post" action="<?php echo esc_url( site_url( 'wp-login.php?action=expiredpassword', 'login_post' ) ); ?>">
 				<input type="hidden" name="user_login" id="user_login" value="<?php echo esc_attr( $user->user_login ); ?>" autocomplete="off" />
 				<input type="hidden" name="key" id="key" value="<?php echo esc_attr( $oncerkey ); ?>" autocomplete="off" />
+				<input type="hidden" name="redirect_to" id="redirect_to" value="<?php echo esc_attr( $redirect_to ); ?>" autocomplete="off" />
 
 				<p>
 					<label for="pass1"><?php _e('New password') ?><br />
@@ -74,10 +143,10 @@ if( !class_exists( 'expirepasswordpublic') ) {
 				</p>
 
 				<div id="pass-strength-result" class="hide-if-no-js"><?php _e('Strength indicator'); ?></div>
-				<p class="description indicator-hint"><?php _e('Hint: The password should be at least seven characters long. To make it stronger, use upper and lower case letters, numbers and symbols like ! " ? $ % ^ &amp; ).'); ?></p>
+				<p class="description indicator-hint"><?php _e('Hint: The password should be at least seven characters long. To make it stronger, use upper and lower case letters, numbers and symbols like ! " ? $ % ^ &amp; ).', 'expirepassword'); ?></p>
 
 				<br class="clear" />
-				<p class="submit"><input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="<?php esc_attr_e('Reset Password'); ?>" /></p>
+				<p class="submit"><input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="<?php esc_attr_e('Reset Password', 'expirepassword'); ?>" /></p>
 			</form>
 			<?php
 
