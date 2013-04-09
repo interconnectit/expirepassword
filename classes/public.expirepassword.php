e61978dc3459bb4a31e501d410509a5acac4445f
shrkey <team@shrkey.com>
2013-04-09 12:53:56 +0100

    added blank public class

--- classes/public.expirepassword.php
+++ classes/public.expirepassword.php
@@ -1,3 +1,23 @@
 <?php
-// Here
-?>
+
+if( !class_exists( 'expirepasswordpublic') ) {
+
+	class expirepasswordpublic {
+
+		function __construct() {
+
+			//add_action( 'init', array( &$this, 'check_for_oncer' ), 1 );
+
+		}
+
+		function expirepasswordpublic() {
+			$this->__construct();
+		}
+
+
+
+	}
+
+}
+
+$expirepasswordpublic = new expirepasswordpublic();
